41954f0cada5e38f411fc7d702e3b025df7347e5
maxim.pestov <maxim.pestov@codezavod.ru>
2019-08-06 18:59:12 +0300

    Оптимизировал, добавил древовидный дебаг

--- classes/DevTools.php
+++ classes/DevTools.php
@@ -4,79 +4,126 @@ namespace Develop;
 
 class DevTools
 {
-    public function dump($variable = null)
-    {
-        echo "<pre style='font-size:10px;
+    private $level = 0;
+    private $style = [
+        'style' => 'font-size:10px;
             position: relative;
             top: 0px;
             text-align:left;
             background: rgba(0, 120, 201, 0.15);
             border: 1px solid #888;
-        '>";
-        $this->filter_tilda_keys($variable);
-        echo "</pre>";
+        ',
+    ];
+
+    public function dump($variable = null)
+    {
+        echo "<pre style='".$this->style['style']."'>";
+        $this->filter_tilda_keys($variable, true);
+        echo '</pre>';
     }
+
 
     private function wrap($color, $text)
     {
-        return "<span style='color:" . $color . ";'>" . htmlspecialchars($text) . "</span>";
+        return '(<span style="color:'.$color.';">'.htmlspecialchars($text).'</span>)';
     }
 
-    private function filter_tilda_keys(&$outputVariable)
+    private function filter_tilda_keys(&$outputVariable, $resetLevel = false)
     {
-        static $level = 0;
-        $tab = "    ";
+        $tab = '    ';
+        $type = gettype($outputVariable);
+
+        echo $this->wrap('magenta', $type);
+
         if (is_array($outputVariable) || is_object($outputVariable)) {
-            if ($level === 0) {
-                echo '(' . $this->wrap('magenta', gettype($outputVariable)) . ') ';
-                echo 'count ' . count($outputVariable);
-                echo "\n";
-                $level++;
-                if (is_object($outputVariable)) {
-                    print_r($outputVariable);
-                } else {
-                    $this->filter_tilda_keys($outputVariable);
-                }
-            } else {
-                foreach ($outputVariable as $key => $value) {
-                    echo str_repeat($tab, $level) . '(' . wrap('magenta', gettype($value)) . ') ';
-                    if (is_array($value) or is_object($value)) {
-                        echo 'count ' . count((array)$value);
-                        echo ' [' . wrap('red', $key) . "]\n";
-                        if (!empty($value)) {
-                            $level++;
-                            if (is_object($value)) {
-                                print_r($outputVariable);
-                            } else {
-                                $this->filter_tilda_keys($value);
-                            }
-                        }
-                    } else {
-                        echo '[' . wrap('black', $key) . "] = ";
-                        $this->checksimpleVariable($value);
-                    }
-                }
-                $level--;
-                echo '';
+            echo 'count '.count($outputVariable);
+            echo "\n";
+            foreach ($outputVariable as $key => $value) {
+                echo str_repeat($tab, $this->level);
+
+                $this->level++;
+                echo $this->wrap('red', $key);
+                echo ' => ';
+                $this->filter_tilda_keys($value);
             }
         } else {
-            echo '(' . $this->wrap('magenta', gettype($outputVariable)) . ') ';
-            $this->checksimpleVariable($outputVariable);
+            $this->showSimple($outputVariable);
+            echo "\n";
         }
 
+        if ($resetLevel === true) {
+            $this->level = 1;
+        } else {
+            $this->level--;
+        }
     }
 
-    private function checksimpleVariable($simpleVariable)
+    private function showSimple($simpleVariable)
     {
         if (is_null($simpleVariable)) {
-            echo 'null' . "\n";
+            echo 'null';
         } elseif (empty($simpleVariable)) {
-            echo "''" . "\n";
+            echo "''";
         } elseif (is_string($simpleVariable)) {
             $simpleVariable = htmlspecialchars($simpleVariable);
-            echo (strlen($simpleVariable) < 40 ? $simpleVariable : substr($simpleVariable, 0, 40) . "…") . "\n";
+            echo(strlen($simpleVariable) < 40 ? $simpleVariable : substr($simpleVariable, 0, 40).'…');
         } else {
-            echo $simpleVariable . "\n";
+            echo $simpleVariable;
+        }
+    }
+
+
+
+    /**
+     * Выводит массив в виде дерева
+     *
+     * @param  mixed - Массив или объект, который надо обойти
+     * @param  boolean - Раскрыть дерево элементов по-умолчанию или нет?
+     *
+     * @return void
+     */
+    public function pretty_print($in, $opened = true)
+    {
+        if ($opened) {
+            $opened = ' open';
+        }
+        if (is_object($in) || is_array($in)) {
+            echo '<div style="'.$this->style['style'].'">';
+            echo '<details'.$opened.'>';
+            echo '<summary>';
+            echo is_object($in) ? 'Object {'.count((array) $in).'}' : 'Array ['.count($in).']';
+            echo '</summary>';
+            $this->pretty_print_rec($in, $opened);
+            echo '</details>';
+            echo '</div>';
+        }
+    }
+
+    private function pretty_print_rec($in, $opened, $margin = 10)
+    {
+        if (!is_object($in) && !is_array($in)) {
+            return;
+        }
+
+        foreach ($in as $key => $value) {
+            if (is_object($value) || is_array($value)) {
+                echo '<details style="margin-left:'.$margin.'px" '.$opened.'>';
+                echo '<summary>';
+                echo is_object($value) ? $key.' {'.count((array) $value).'}' : $key.' ['.count($value).']';
+                echo '</summary>';
+                $this->pretty_print_rec($value, $opened, $margin + 10);
+                echo '</details>';
+            } else {
+                switch (gettype($value)) {
+                    case 'string':
+                        $bgc = 'red';
+                        break;
+                    case 'integer':
+                        $bgc = 'green';
+                        break;
+                }
+                echo '<div style="margin-left:'.$margin.'px">'.$key.' : <span style="color:'.$bgc.'">'.$value.'</span> ('.gettype($value).')</div>';
+            }
         }
     }
 
--- index.php
+++ index.php
@@ -5,4 +5,29 @@ require $_SERVER['DOCUMENT_ROOT'].'/classes/DevTools.php';
 use Develop\DevTools;
 $devTools = new DevTools();
 //phpinfo();
-$devTools->dump();
+
+$testVar = 1;
+$testVar2 = 'string';
+$testVar3 = new DateTime();
+$testVar4 = [
+    [
+        0 => [
+            'test' => $testVar2,
+        ],
+        'test2' => $testVar,
+    ],
+    [
+        'test' => $testVar2
+    ],
+    [
+        'test' => new DateTime(),
+    ],
+    'yes' => false
+];
+
+$devTools->dump($testVar);
+$devTools->dump($testVar2);
+$devTools->dump($testVar3);
+$devTools->dump($testVar4);
+$devTools->pretty_print($testVar4, false);
+$devTools->pretty_print($testVar4);
